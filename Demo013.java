c75778da1a32e66740ba788d89b5b8fa7aaaa180
Srivardhan Reddy <srivardhannandikonda@gmail.com>
2025-01-07 12:49:08 +0530

    Add files via upload

---
+++ Demo013.java
@@ -0,0 +1,8 @@
+public class Demo013 {
+    public static void main(String[] args){
+        int base = 7;
+        int height = 10;
+        double area = 0.5*base*height;
+        System.out.println("Area of triangle: "+ area);
+    }
+}
