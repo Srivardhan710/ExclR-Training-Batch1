cbadd1ee003f6d3bed6f27e36dfe66846d11cdc6
Srivardhan Reddy <srivardhannandikonda@gmail.com>
2025-01-07 12:29:02 +0530

    Add files via upload

---
+++ Demo5.java
@@ -0,0 +1,10 @@
+public class Demo5 {
+    public static void main(String[] args){
+        String username = "Srivardhan";
+        String pss = "Bunny010";
+        while ((username == "Srivardhan")&&(pss == "Buny010")){
+            System.out.println("Hello");
+            break;
+        }
+    }
+}
