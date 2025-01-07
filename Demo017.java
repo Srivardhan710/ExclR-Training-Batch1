fd11f296abc1c4a76ee53feb1fe25809b954d23b
Srivardhan Reddy <srivardhannandikonda@gmail.com>
2025-01-07 13:20:04 +0530

    Add files via upload

---
+++ Demo017.java
@@ -0,0 +1,8 @@
+public class Demo017 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 10;
+        int sum = a+b;
+        System.out.println("Sum of 2 digits is: "+sum);
+    }
+}
