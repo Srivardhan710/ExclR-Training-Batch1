715eb1554887ca8689632c50baf31e75fb944601
Srivardhan Reddy <srivardhannandikonda@gmail.com>
2025-01-07 12:48:50 +0530

    Add files via upload

---
+++ Demo012.java
@@ -0,0 +1,8 @@
+public class Demo012 {
+        public static void main(String[] args){
+            int radius = 7;
+            double area = Math.PI * Math.pow(radius, 2);
+            System.out.println("Area of the Circle "+ radius + "is"+ area);
+        }
+}
+    
