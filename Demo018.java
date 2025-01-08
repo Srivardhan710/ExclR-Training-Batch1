c32f09cbfdbf14563aabda6c03e612623f68ae89
Srivardhan Reddy <srivardhannandikonda@gmail.com>
2025-01-08 13:20:04 +0530

    Add files via upload

---
+++ Demo018.java
@@ -0,0 +1,7 @@
+public class Demo018 {
+    public static void main(String[] args) {
+        int number = 107;
+        int nextMultiple = (number / 100 + 1) * 100;
+        System.out.println("The next multiple of 100 is: " + nextMultiple);
+    }
+}
