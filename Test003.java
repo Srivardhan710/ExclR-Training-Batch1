b1d1c60d5fd31f547ce34b7d1c2d2c721384a85f
Srivardhan Reddy <srivardhannandikonda@gmail.com>
2025-01-10 13:19:12 +0530

    Add files via upload

---
+++ Test003.java
@@ -0,0 +1,13 @@
+public class Test003 {
+    String itemname;
+    int item;
+    double price;
+    double total;
+public Test003(){
+    this.itemname = "itemname";
+    this.item = 0;
+    this.price = 0.0;
+    this.total = 0.0;
+
+}
+}
