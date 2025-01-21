19872be84d36fe872bf2b71c85c8e979bf3f8fb9
Srivardhan Reddy <srivardhannandikonda@gmail.com>
2025-01-21 09:56:26 +0530

    Add files via upload

---
+++ Demo029.java
@@ -0,0 +1,15 @@
+
+public class Demo029 {
+    public static void main(String[] args) {
+        String[] friendsNames = {"Saiteja", "Sravan", "Charan", "Shivani", "Deepika","kumar","Sai","Aaradhya","Ria","Saiteja"};
+        long[] friendsPhones = {1234567891L, 1234567895L, 1234567896L, 1234567894L, 1234567898L,1234567892L,1234567893L,1234567899L,1234567896L,1234567896L};
+        System.out.println("My friends' names and their phone numbers are:");
+        for (int i = 0; i < friendsNames.length; i++) {
+            System.out.println("Friend's name: " + friendsNames[i] + ", Phone number: " + friendsPhones[i]);
+            
+        }
+
+        
+    }
+    
+}
