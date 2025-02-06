78cceb49b5fb4f8ff6e9ac51525d1f97e7d28dce
Srivardhan Reddy <srivardhannandikonda@gmail.com>
2025-02-06 10:51:12 +0530

    Array Elements

---
+++ Demo065.java
@@ -0,0 +1,9 @@
+public class Demo065 {
+    public static void main(String[] args) {
+        int array[] = {11,22,33,44,55,66,77};
+        System.out.println("Array Elements: ");
+        for(int i=0; i<array.length; i++){
+        System.out.print(array[i]+ " "); 
+        } 
+    }
+}
